a1aae967ee377d6019b0efe0be1fcb527b80ec17
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-09-27 16:42:04 -0700

    Created Hero Section. Created Button component and Imported marker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header />
+      <Hero />
     </main>
   )
 }
